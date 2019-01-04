2154ac04d38ee2c928e320367383aaab60f38b4b
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-03 22:01:39 -0800

    Challenge-2 completed

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,7 +6,37 @@ namespace Prework_CodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool incorrect = true;
+            do
+            {
+            Console.WriteLine("Type in to see if the year is a leap year or not!");
+            string yearInput = Console.ReadLine();
+            int year = Convert.ToInt32(yearInput);
+
+            if((year % 4 == 0)) 
+            {
+                if (year % 100 != 0)
+                {
+                        
+                        Console.Write("This is a leap year");
+                }
+                else if (year % 400 == 0)
+                {
+
+                    Console.Write("This is a leap year");
+                }
+                else
+                {
+                    Console.Write("Sorry not a leap year");
+                }
+                    Console.ReadLine();
+            }
+            
+
+            } while (incorrect);
+
+
+            
         }
     }
 }
