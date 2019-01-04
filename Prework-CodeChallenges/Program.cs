ccc13dae0682f0be30cb50e41bcaedd2006011ca
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-04 07:11:03 -0800

    Challenge-3 completed

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -6,7 +6,49 @@ namespace Prework_CodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Input 3 random numbers to find out if you have a perfect sequence?");
+
+                string[] numbersArray = new string[3];
+
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+
+                    numbersArray[i] = Console.ReadLine();
+
+                }
+
+                int[] myInts = Array.ConvertAll(numbersArray, int.Parse);
+
+
+
+
+                int sum = 0, prod = 1;
+
+                foreach (int value in myInts)
+
+                {
+                    sum += value;
+                    prod *= value;
+
+                }
+                if (sum == prod)
+
+                {
+                    Console.WriteLine("This is a perfect sequence");
+                }
+                else
+                {
+                    Console.WriteLine("This is not a perfect sequence");
+                }
+
+
+            } while (incorrect);
+            
+
         }
     }
 }
